19f2caa63dfcfe6d0ffe1db47bf83eb030787cfb
Luca Bruno <luca.bruno@coreos.com>
2018-10-19 09:55:47 +0000

    lib: clean all clippy warnings
    This cleans all remaining clippy warnings and enables stricter check
    on Travis.

--- examples/inspect_rsa.rs
+++ examples/inspect_rsa.rs
@@ -1,10 +1,11 @@
+extern crate dirs;
 extern crate openssh_keys;
 
-use std::{env, fs, io, path};
+use std::{fs, io, path};
 use std::io::BufRead;
 
 fn main() {
-    let home = env::home_dir().unwrap_or_else(|| path::PathBuf::from("/home/core/"));
+    let home = dirs::home_dir().unwrap_or_else(|| path::PathBuf::from("/home/core/"));
     let pub_path = home.join(".ssh").join("id_rsa.pub");
     println!("Inspecting '{}':", pub_path.to_string_lossy());
     let file = fs::File::open(&pub_path).expect("unable to open RSA pubkey");
